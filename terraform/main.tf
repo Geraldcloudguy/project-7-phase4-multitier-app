faee02e5586dce21ce820c8c5a5019e8be16f123
Geraldcloudguy <Geraldpena94@gmail.com>
2026-02-20 10:24:04 -0500

    Add VPC and public/private subnets

---
+++ terraform/main.tf
@@ -0,0 +1,43 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.0"
+    }
+  }
+}
+
+provider "aws" {
+  region = "us-east-1"
+}
+
+# --- VPC ---
+resource "aws_vpc" "main" {
+  cidr_block           = "10.0.0.0/16"
+  enable_dns_support   = true
+  enable_dns_hostnames = true
+  tags = {
+    Name = "phase4-multitier-vpc"
+  }
+}
+
+# --- Public Subnet ---
+resource "aws_subnet" "public" {
+  vpc_id                  = aws_vpc.main.id
+  cidr_block              = "10.0.1.0/24"
+  map_public_ip_on_launch = true
+  availability_zone       = "us-east-1a"
+  tags = {
+    Name = "public-subnet"
+  }
+}
+
+# --- Private Subnet ---
+resource "aws_subnet" "private" {
+  vpc_id            = aws_vpc.main.id
+  cidr_block        = "10.0.2.0/24"
+  availability_zone = "us-east-1a"
+  tags = {
+    Name = "private-subnet"
+  }
+}
